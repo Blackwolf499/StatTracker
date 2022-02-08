a1c91c1b561a134591c8aac3d1e4e906437341e2
Blackwolf499 <kirwinwebb@gmail.com>
2022-02-08 20:40:09 +1100

    Fixed bug where rank won't recursively update

--- rank-tracker.py
+++ rank-tracker.py
@@ -35,26 +35,25 @@ browser.get(url)
 flag = True
 
 # Fetching appropriate data for logging
-def data_fetch(val):
+def data_fetch():
     rank = browser.find_element(By.ID, "user-rank").text
-    percentile = browser.find_element(By.ID, "profile-rank-badge").text
-    if(val == "rank"):
-        return rank
-    else:
-        return percentile
+
+    # Not used for now but may be implemented later
+    # percentile = browser.find_element(By.ID, "profile-rank-badge").text
+    return rank
 
 # Reading score before writing (to prevent duplicates)
 def last_score_stored():
     with open("pandas_data.csv", "r") as file:
         lines = file.read().splitlines()
         rank_saved = int(list(lines[-1].split(" "))[0])
-        file.close()
     return int(rank_saved)
 
 # Writing data to a file in a while loop:
 while flag:
     # Calling data_fetch() func to periodically update rank & percentile
-    rank = data_fetch("rank")
+    browser.refresh()
+    rank = data_fetch()
     
     # Comparing updated score to lastest saved score in rank-data.txt
     # If the value is different it will append it to file, avoiding duplicate data
@@ -70,11 +69,7 @@ while flag:
 
             # Close file to avoid erroring out
             file.close()
-            
-            # Update last_score_stored
-            last_score_stored()
-
             print("Score updated! Position changed by: " + str(pos_dif))    
     else:
         print("Score hasn't changed, sleeping for 180s...")
-        time.sleep(180)
+        time.sleep(5)
